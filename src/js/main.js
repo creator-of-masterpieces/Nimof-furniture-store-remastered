771f754211973a1b1126298940505b3b4ceb1931
Dmitrii Timonov <42276523+creator-of-masterpieces@users.noreply.github.com>
2025-05-26 18:13:04 +0300

    Подключил библиотеку с svg иконками lucide; Добавил в разметку секцию benefits; Начал стилизацию секции benefits;

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 // Общий JS для сайта
+
+// Инициализировал svg иконки
+document.addEventListener('DOMContentLoaded', () => {
+    lucide.replace({
+        stroke: 'currentColor',
+        fill: 'currentColor',
+        width: 24,
+        height: 24
+    });
+});
